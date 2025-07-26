72edadce823f4624a91365d851aefdbc042e071b
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2025-07-26 22:33:46 +0545

    updated settings.py

--- python1shopapp/settings.py
+++ python1shopapp/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onepythonshopapp.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Defender Configuration (SQLite only)
