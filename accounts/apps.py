1975693c563834d700273c45ce708140eab4e76e
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2025-12-05 18:47:01 +0545

    render superuser created to add images

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,31 @@
 from django.apps import AppConfig
+import os
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        from django.db.utils import OperationalError, ProgrammingError
+
+        username = os.environ.get('RENDER_SUPERUSER_USERNAME')
+        email = os.environ.get('RENDER_SUPERUSER_EMAIL')
+        password = os.environ.get('RENDER_SUPERUSER_PASSWORD')
+
+        # Only run if all env vars are present
+        if not (username and email and password):
+            return
+
+        User = get_user_model()
+        try:
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(
+                    username=username,
+                    email=email,
+                    password=password,
+                )
+        except (OperationalError, ProgrammingError):
+            # DB not ready during migrations/startup – ignore
+            pass
--- python1shopapp/settings.py
+++ python1shopapp/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'category',
-    'accounts',
+    'accounts.apps.AccountsConfig',
     'store',
     'carts',
     'orders',
